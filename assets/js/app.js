ab0e59a4da2908ed0e1c325ee89e9b629c87555a
shafik720 <shafikrasel5@gmail.com>
2022-09-26 11:49:38 +0600

    dfgsdfg

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,20 @@
+// getting all document values
 let stopWatchNumber = document.getElementById('stopWatchNumber');
 let stopWatchDiv = document.getElementById('stopWatchDiv');
+let anserDiv1 = document.getElementById('anserDiv1');
+let anserDiv2 = document.getElementById('anserDiv2');
+let anserDiv3 = document.getElementById('anserDiv3');
+let anserDiv4 = document.getElementById('anserDiv4');
+let showGameNumber = document.getElementById('showGameNumber');
+let showFirstNumber = document.getElementById('firstNumber');
+let showSecondNumber = document.getElementById('secondNumber');
 
 
+// Making the Start button functional
 document.getElementById('startBtn').addEventListener('click',function(){
-    stopWatch(10);
+    // stopWatch(10);
+    randomizeInBoxes();
+    randomNumberInMainBox();
 })
 
 
@@ -30,4 +41,39 @@ const stopWatch = (startingPoint) => {
             clearInterval(x);
         }
     },1000)
+}
+
+const randomizeInBoxes = () => {
+    let randomNumberArray = [];
+    let boxArray = [anserDiv1,anserDiv2,anserDiv3,anserDiv4];
+    for(let i=0;i<4;i++){        
+        randomNumberArray.push(Math.ceil(Math.random() * 100));
+    }
+    for(let i=0;i<boxArray.length; i++){
+        boxArray[i].innerHTML = `
+            <h3>${randomNumberArray[i]}</h3>
+        `;
+    }
+    
+    console.log (boxArray);
+}
+
+const randomNumberInMainBox = () => {
+    
+    function forFirstNumber(){
+        let x = Math.ceil(Math.random()*10);
+        if(x===0 || x===1 || x>10){
+            return forFirstNumber();
+        }
+        showFirstNumber.innerText = x;
+    }
+    function forSecondNumber(){
+        let x = Math.ceil(Math.random()*10);
+        if(x===0 || x===1 || x>10){
+            return forSecondNumber();
+        }
+        showSecondNumber.innerText = x;
+    }
+    forFirstNumber();
+    forSecondNumber();
 }
